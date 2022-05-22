313975d5eb68695c11169dbe959a90e3f93d8353
Redempt <instantiable@gmail.com>
2022-05-22 12:24:47 -0400

    Update CommandDispatcher to take context instead of sender and string args

--- src/redempt/ordinate/processing/CommandDispatcher.java
+++ src/redempt/ordinate/processing/CommandDispatcher.java
@@ -1,7 +1,9 @@
 package redempt.ordinate.processing;
 
+import redempt.ordinate.data.CommandContext;
+
 public interface CommandDispatcher<T> {
 	
-	public void dispatch(T sender, String[] args);
+	public void dispatch(CommandContext<T> context);
 	
 }
