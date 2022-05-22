0b82d58561f46563fbf0e0caec8cafb70d0d333f
Imagine <70789387+ImajinDevon@users.noreply.github.com>
2022-05-22 16:55:59 -0400

    SplittableList rework (#1)
    * SplittableList.java O(1) rework
    
    * Allow SplittableList<T> to skip one index over the last element, instead of one before

--- src/redempt/ordinate/data/SplittableList.java
+++ src/redempt/ordinate/data/SplittableList.java
@@ -1,132 +1,64 @@
 package redempt.ordinate.data;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.Spliterator;
-import java.util.function.Consumer;
+public class SplittableList<T> {
 
-public class SplittableList<T> implements Iterable<T> {
-	
-	private T[] array;
-	private boolean[] removed;
-	private int removedCount = 0;
+	private Object[] array;
+
 	private int start;
-	
+
 	public SplittableList(T[] array, int start) {
 		this.array = array;
 		this.start = start;
 	}
-	
+
 	public SplittableList(T[] array) {
 		this(array, 0);
 	}
-	
+
+	public void remove(int rawIndex) {
+		int index = rawIndex + start;
+
+		Object[] newArray = new Object[array.length - 1];
+		System.arraycopy(array, start, newArray, 0, index);
+		System.arraycopy(array, index + 1, newArray, index, array.length - start - 1);
+		array = newArray;
+		start = 0;
+	}
+
 	public void skip(int amount) {
-		int counter = 0;
-		for (int i = start; i < array.length; i++) {
-			if (!isRemoved(i) && counter++ == amount) {
-				start = i;
-				return;
-			}
-		}
+		start += amount;
+		start = Math.min(start, array.length);
 	}
-	
-	private boolean isRemoved(int index) {
-		return removed != null && removed[index];
-	}
-	
+
 	public T peek() {
 		if (hasNext()) {
-			return array[start];
+			return (T) array[start];
 		}
 		return null;
 	}
-	
+
 	public T poll() {
-		while (isRemoved(start)) start++;
-		T val = peek();
+		Object val = peek();
 		if (val != null) {
 			start++;
 		}
-		return val;
+		return (T) val;
 	}
-	
+
 	public boolean hasNext() {
-		return start < array.length - removedCount;
+		return start < array.length;
 	}
-	
+
 	public int size() {
-		return array.length - start - removedCount;
+		return array.length - start;
 	}
-	
+
 	public T get(int index) {
-		return array[trueIndex(index)];
+		return (T) array[start + index];
 	}
-	
-	private int trueIndex(int index) {
-		int counter = 0;
-		for (int i = start; i < array.length; i++) {
-			if (!isRemoved(i) && counter++ == index) {
-				return i;
-			}
-		}
-		return array.length;
-	}
-	
-	public void remove(int index) {
-		if (removed == null) {
-			removed = new boolean[array.length];
-		}
-		removedCount++;
-		index = trueIndex(index);
-		removed[index] = true;
-	}
-	
+
 	public SplittableList<T> split(int newStart) {
-		SplittableList<T> list = new SplittableList<>(array, start + newStart);
-		if (removed != null) {
-			list.removed = Arrays.copyOf(removed, removed.length);
-		}
-		return list;
+		return new SplittableList(array, start + newStart);
 	}
-	
-	@Override
-	public Iterator<T> iterator() {
-		return new Iterator<T>() {
-			
-			int index = start;
-			
-			private void advanceToNext() {
-				while (isRemoved(index)) index++;
-			}
-			
-			@Override
-			public boolean hasNext() {
-				advanceToNext();
-				return index < array.length;
-			}
-			
-			@Override
-			public T next() {
-				advanceToNext();
-				return array[index++];
-			}
-			
-		};
-	}
-	
-	@Override
-	public void forEach(Consumer<? super T> action) {
-		for (int i = start; i < array.length; i++) {
-			if (!isRemoved(i)) {
-				action.accept(array[i]);
-			}
-		}
-	}
-	
-	@Override
-	public Spliterator<T> spliterator() {
-		return Iterable.super.spliterator();
-	}
-	
+
 }
