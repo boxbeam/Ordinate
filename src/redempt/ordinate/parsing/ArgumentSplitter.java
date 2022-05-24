4a642fefb41eb91152f203b69a5ebff461c6f825
Redempt <instantiable@gmail.com>
2022-05-23 23:05:35 -0400

    Implement ArgumentSplitter

--- src/redempt/ordinate/parsing/ArgumentSplitter.java
+++ src/redempt/ordinate/parsing/ArgumentSplitter.java
@@ -1,7 +1,45 @@
 package redempt.ordinate.parsing;
 
+import redempt.ordinate.data.Argument;
+import redempt.ordinate.data.SplittableList;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArgumentSplitter {
 
+	public static SplittableList<Argument> split(String[] command) {
+		return split(String.join(" ", command));
+	}
 
+	public static SplittableList<Argument> split(String command) {
+		List<Argument> args = new ArrayList<>();
+		boolean quoted = false;
+		StringBuilder buffer = new StringBuilder();
+		for (int i = 0; i < command.length(); i++) {
+			char c = command.charAt(i);
+			if (c == '\\' && i + 1 < command.length() && command.charAt(i + 1) == '"') {
+				i++;
+				buffer.append('"');
+				continue;
+			}
+			if (c == '"') {
+				quoted = !quoted;
+				if (!quoted) {
+					args.add(new Argument(buffer.toString(), true));
+					buffer.setLength(0);
+				}
+				continue;
+			}
+			if (c == ' ' && !quoted && buffer.length() > 0) {
+				args.add(new Argument(buffer.toString(), false));
+				buffer.setLength(0);
+				continue;
+			}
+			buffer.append(c);
+		}
+		Argument[] array = args.toArray(new Argument[0]);
+		return new SplittableList<>(array);
+	}
 
 }
