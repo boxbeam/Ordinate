8b0774f9f1d84e5c35e82b1ced1dbac38cdd6150
Redempt <instantiable@gmail.com>
2022-05-29 19:26:55 -0400

    Create lexer in CommandParser

--- src/redempt/ordinate/parser/CommandParser.java
+++ src/redempt/ordinate/parser/CommandParser.java
@@ -1,8 +1,24 @@
 package redempt.ordinate.parser;
 
 import redempt.ordinate.dispatch.CommandManager;
+import redempt.redlex.bnf.BNFParser;
+import redempt.redlex.processing.CullStrategy;
+import redempt.redlex.processing.Lexer;
+
+import java.io.InputStream;
 
 public class CommandParser<T> {
+
+	private static  Lexer lexer;
+
+	static {
+		InputStream inp = CommandParser.class.getClassLoader().getResourceAsStream("ordn.bnf");
+		lexer = BNFParser.createLexer(inp);
+		lexer.setUnnamedRule(CullStrategy.LIFT_CHILDREN);
+		lexer.setRetainEmpty(false);
+		lexer.setRetainStringLiterals(false);
+		lexer.setRuleByName(CullStrategy.DELETE_ALL, "sep", "break");
+	}
 
 	private ParserOptions<T> options;
 	private CommandManager<T> manager;
